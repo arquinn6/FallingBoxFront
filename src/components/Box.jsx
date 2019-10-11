abdf5d9ed5255ff3a76de300b6ecf8d7d4fa2938
arquinn6 <arquinn6@gmail.com>
2019-10-11 09:18:30 -0600

    start

---
+++ src/components/Box.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Box extends Component{
+    constructor(props){
+        super(props)
+        this.state ={
+        }
+    }
+}
---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class GameBoard extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+        }
+    }
+}
