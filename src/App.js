f512bca15c0c75be3723c94bf46e00421b9cbecd
gpwade <39201050+gpwade@users.noreply.github.com>
2019-10-11 16:39:35 -0600

    Reworking player jsx
    Want to make it a more fluid process

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Fragment} from 'react';
 import board from './GameSprites/GameBoard.png'
 import block from './GameSprites/FallingBlock.png';
-import playerObject from './GameSprites/PlayerCharacter.png'
 import Player from './components/Player'
 import './App.css';
 
@@ -10,10 +9,6 @@ function App() {
 	 <Fragment>
 		<div className="Game">
 			<img src={board} className="Game-Board" alt="Upsidedown_Smiling_Face" /> 
-		</div>
-		
-		<div className="Player" style={{left:'50%', top:'75%'}}>
-			<img src={playerObject} className="playerObject" alt="playerObject"/>
 		</div>
 		
 		< Player />
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,103 +1,199 @@
 import React, {Component} from 'react'
+import playerObject from '../GameSprites/PlayerCharacter.png'
+import './Player.css'
 
 class Player extends Component{
 	constructor(props) {
 		super(props)
 		this.state = {
-			leftKey : {
-				counter : 0, 
+			left: false,
+			right: false,
+			up: false,
+			player : {
+				left: '50%',
+				leftVelocity: 0,
+				top: '90%',
+				topVelocity: 0
 			},
-			rightKey : {
-				counter : 0, 
-			},
-			upKey : {
-				counter : 0, 
-			},
-			position : {
-				leftMargin : 25,
-				topMargin : 75,
-			}
+			//player: document.getElementsByClassName('Player')[0],
+			
+			
+			// leftKey : {
+				// counter : 0, 
+			// },
+			// rightKey : {
+				// counter : 0, 
+			// },
+			// upKey : {
+				// counter : 0, 
+			// },
+			// position : {
+				// leftMargin : 50,
+				// topMargin : 75,
+			// }
 		}
 	}
 	
-	onKeyDownHandler = (key) => {	
-		let copy = this.state
-		var el = document.getElementsByClassName('Player')
-		console.log(el[0])
-		switch (key) {
-			case ('ArrowLeft'):
-			case ('a'):
-				copy.leftKey.counter += 1
-				
-				if (copy.position.leftMargin*2 > 5) {
-					copy.position.leftMargin -= 1;
-					el[0].style.left = copy.position.leftMargin*2+'%';
-				}
-				break
-			case ('ArrowRight'):
-			case ('d'):
-				copy.rightKey.counter += 1
-				
-				if (copy.position.leftMargin*2 < 85) {
-					copy.position.leftMargin += 1
-					el[0].style.left = copy.position.leftMargin*2+'%'
-				}
-				break
-			case ('ArrowUp'):
-			case ('w'):
-				copy.upKey.counter += 1
-				if (copy.rightKey.counter > 0) {
-					this.jumpRight()
-				}
-				if (copy.leftKey.counter > 0) {
-					this.jumpLeft()
-				}
-				copy.position.topMargin -= 1
-				el[0].style.top = copy.position.topMargin*1+'%'
-				break
-			default: break
+	KeyChange = (e) => {
+		var keyState = e.type === 'keydown' ? true : false
+		switch (e.code) {
+			case 'ArrowUp':
+			case 'w':
+				this.setState({up: keyState})
+				break;
+			case 'ArrowRight':
+			case 'd':
+				this.setState({right: keyState})
+				break;
+			case 'ArrowLeft':
+			case 'a':
+				this.setState({left: keyState})
+				break;
+			default: break;
 		}
-		this.setState({ copy })
+	}
+	
+	loop = () => {
+		let copy = this.state
+		//window.requestAnimationFrame(this.loop)
+		if (copy.up && copy.player.style.jumping === false) {
+			console.log(copy.player.style)
+			console.log("Jump!")
+			copy.player.style.topVelocity -= 20
+			copy.player.style.jumping = true
+		}
+		if (copy.left) {
+			copy.player.leftVelocity -= .5
+			console.log("left")
+		}
+		if (copy.right) {
+			copy.player.leftVelocity += .5
+			console.log("right")
+		}
+
+		copy.player.style.topVelocity += 1.5
+		copy.player.style.left = (copy.player.style.leftVelocity + copy.player.style.left) + '%'
+		copy.player.style.top = (copy.player.style.topVelocity + copy.player.style.top) + '%'
+		copy.player.style.leftVelocity *= .9
+		copy.player.style.topVelocity *= .9
+		
+		
+		this.setState(copy)
 	}
 
-	onKeyUpHandler = (key) => {
-		let copy = this.state
-		
-		switch (key) {
-			case ('ArrowLeft'):
-			case ('a'):
-				copy.leftKey.counter = 0
-				break
-			case ('ArrowRight'):
-			case ('d'):
-				copy.rightKey.counter = 0
-				break
-			case ('ArrowUp'):
-			case ('w'):
-				copy.upKey.counter = 0
-				break
-			default: break
-		}
-		
-		this.setState({ copy })
-	}
 	
-	jumpRight = () => {
-		console.log("Jump Right")
+
+	// onKeyDownHandler = (key) => {	
+		// let copy = this.state
+		// var el = document.getElementsByClassName('Player')[0]
 		
-	}
+		// switch (key) {
+			// case ('ArrowLeft'):
+			// case ('a'):
+				// copy.leftKey.counter += 1
+				// if (copy.position.leftMargin > 5) {
+					// copy.position.leftMargin -= 5;
+					// el.style.left = copy.position.leftMargin+'%';
+				// }
+				// break
+			// case ('ArrowRight'):
+			// case ('d'):
+				// copy.rightKey.counter += 1
+				
+				// if (copy.position.leftMargin < 90) {
+					// copy.position.leftMargin += 5
+					// el.style.left = copy.position.leftMargin+'%'
+				// }
+				// break
+			// case ('ArrowUp'):
+			// case ('w'):
+				// copy.upKey.counter += 1
+				// if (copy.rightKey.counter > 0) {
+					// this.jumpRight(el, copy)
+				// }
+				// if (copy.leftKey.counter > 0) {
+					// this.jumpLeft(el, copy)
+				// }
+				// copy.position.topMargin -= 5
+				// el.style.top = copy.position.topMargin*1+'%'
+				// break
+			// default: break
+		// }
+		// this.setState({ copy })
+	// }
+
+	// onKeyUpHandler = (key) => {
+		// let copy = this.state
+		
+		// switch (key) {
+			// case ('ArrowLeft'):
+			// case ('a'):
+				// copy.leftKey.counter = 0
+				// break
+			// case ('ArrowRight'):
+			// case ('d'):
+				// copy.rightKey.counter = 0
+				// break
+			// case ('ArrowUp'):
+			// case ('w'):
+				// copy.upKey.counter = 0
+				// break
+			// default: break
+		// }
+		
+		// this.setState({ copy })
+	// }
+
 	
-	jumpLeft = () => {
-		console.log("Jump Left")
+	// jumpRight = (el, copy) => {
+		// var i = 0
+		// setTimeout(function () {
+			
+			// if (i < 10) {
+				// if (copy.position.leftMargin < 90) { copy.position.leftMargin += i }
+				// copy.position.topMargin -= 5
+				// el.style.top = copy.position.topMargin+'%'
+				// el.style.left = copy.position.leftMargin+'%'
+				
+		// }i++}, 100)
 		
+	// }
+	
+	// jumpLeft = (el, copy) => {
+		
+		// // for (var i = 0; i < 5; i++) {
+			// // console.log(i)
+			// // el.style.pos
+			// // if (copy.position.leftMargin > 5) { copy.position.leftMargin -= i}
+			// // copy.position.topMargin -= 1
+			// // el.style.left = copy.position.leftMargin+'%'
+			// // el.style.top = copy.position.topMargin+'%'
+		// // }
+		
+	// }
+	
+	componentDidMount() {
+		window.addEventListener('keydown', this.KeyChange)
+		window.addEventListener('keyup', this.KeyChange)
+		window.requestAnimationFrame(this.loop)
+		this.setState({player : document.getElementsByClassName('Player')[0]})
 	}
 	
     render () {
 		return (
-			<div
-				tabIndex={-1}
-				onKeyDown={event => this.onKeyDownHandler(event.key)}
-				onKeyUp={event => this.onKeyUpHandler(event.key)}>
+			<div className="Player" 
+				style={{
+					jumping: false,
+					left:this.state.player.left, 
+					top:this.state.player.top,
+					leftVelocity:this.state.player.leftVelocity,
+					topVelocity:this.state.player.topVelocity
+				}}
+				//onKeyChange={event => this.onKeyChange(event.key)}
+				//onKeyDown={event => this.onKeyDownHandler(event.key)}
+				//onKeyUp={event => this.onKeyUpHandler(event.key)}
+				tabIndex="0">
+				<img src={playerObject} className="playerObject" alt="playerObject"/>
 			</div>
 		)
 	}
